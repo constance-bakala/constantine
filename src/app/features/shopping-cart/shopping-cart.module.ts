c6a9ad717bef5bee5fe9a86f28914d391c1276b7
wylfrand <aristide.mvou-ext@malakoffhumanis.com>
2020-07-10 11:51:25 +0200

    Updated version

--- src/app/features/shopping-cart/shopping-cart.module.ts
+++ src/app/features/shopping-cart/shopping-cart.module.ts
@@ -3,12 +3,10 @@ import {CartItemsComponent} from './cart-items/cart-items.component';
 import {SharedModule} from '@shared/shared.module';
 import {ShoppingCartRoutingModule} from '@app/features/shopping-cart/shopping-cart.routing.module';
 import {AuthModule} from '@app/auth/auth.module';
-import {BasketItemComponent} from '@app/features/shopping-cart/cart-items/basket-item/basket-item.component';
 
 
 @NgModule({
-  declarations: [CartItemsComponent,
-    BasketItemComponent],
+  declarations: [CartItemsComponent],
   imports: [
     SharedModule,
     ShoppingCartRoutingModule,
