78b1811637ded0c7489d77f55de9d8af352f6e65
wylfrand <delice.eternel.gabon@gmail.com>
2021-05-23 00:05:17 +0200

    Updated firebase function configuration

--- src/app/auth/main/main.component.ts
+++ src/app/auth/main/main.component.ts
@@ -7,8 +7,8 @@ import {environment as env} from '@env/environment';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  logoFondClair = require('../../../assets/portfolio/boucle_oreille_sans_arriere_plan.png');
-  logoGroup = require('../../../assets/portfolio/photo_maman2-removebg-preview.png');
+  // logoFondClair = require('../../../assets/portfolio/boucle_oreille_sans_arriere_plan.png');
+  // logoGroup = require('../../../assets/portfolio/photo_maman2-removebg-preview.png');
   envName = env.envName;
   version = env.versions.app;
   isProd = env.production;
