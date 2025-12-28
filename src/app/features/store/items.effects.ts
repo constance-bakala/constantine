057a9f211af2cc45e7c60463cbb99c59b0ee69a1
delice.eternel.gabon@gmail.com <delice.eternel.gabon@gmail.com>
2025-12-28 15:57:04 +0100

    migration angular 15

--- src/app/features/store/items.effects.ts
+++ src/app/features/store/items.effects.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
-import {Actions, Effect, ofType} from '@ngrx/effects';
-import {Action, Store} from '@ngrx/store';
-import {Observable, of} from 'rxjs';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {of} from 'rxjs';
 import {catchError, map, switchMap} from 'rxjs/operators';
 import {ItemsService} from '@app/features/items.service';
 import {
@@ -10,42 +9,34 @@ import {
   ActionItemsRetrieveSuccess,
   ActionItemToogleSelect,
   ActionItemToogleSelectSuccess,
-  ItemsActionTypes
+  ItemsActionTypes,
 } from '@app/features/store/items.actions';
+import {Category} from "@shared/interfaces";
 
 @Injectable()
 export class ItemsEffects {
-  constructor(private actions$: Actions,
-              private store$: Store<any>,
-              private itemsService: ItemsService) {
+  constructor(
+    private actions$: Actions,
+    private itemsService: ItemsService
+  ) {
   }
 
-  @Effect()
-  retriveAll(): Observable<Action> {
-    return this.actions$
-      .pipe(
-        ofType(ItemsActionTypes.RETRIEVE_ITEMS),
-        switchMap((action: ActionItemsRetrieve) =>
-          this.itemsService
-            .findAllFromAssets(action.payload.category)
-            .pipe(
-              map(response => new ActionItemsRetrieveSuccess(response)),
-              catchError(error =>
-                of(new ActionItemsRetrieveError({error}))
-              )
-            )
+  retriveAll$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ItemsActionTypes.RETRIEVE_ITEMS),
+      switchMap((action: ActionItemsRetrieve) =>
+        this.itemsService.findAllFromAssets(action.payload.category).pipe(
+          map((response: Category) => new ActionItemsRetrieveSuccess(response)),
+          catchError((error) => of(new ActionItemsRetrieveError({error})))
         )
-      );
-  }
+      )
+    )
+  );
 
-  @Effect()
-  toogleSelectOneItem(): Observable<Action> {
-    return this.actions$
-      .pipe(
-        ofType(ItemsActionTypes.TOOGLE_SELECT_ITEM),
-        map((action: ActionItemToogleSelect) => {
-          return new ActionItemToogleSelectSuccess(action.payload);
-        })
-      );
-  }
+  toogleSelectOneItem$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ItemsActionTypes.TOOGLE_SELECT_ITEM),
+      map((action: ActionItemToogleSelect) => new ActionItemToogleSelectSuccess(action.payload))
+    )
+  );
 }
