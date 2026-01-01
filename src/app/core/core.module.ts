762f46d1b891d90d8a331fbfdf18f09270c6fece
delice.eternel.gabon@gmail.com <delice.eternel.gabon@gmail.com>
2026-01-02 00:21:52 +0100

    Réparation basket page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 import {MetaReducer, StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
@@ -17,6 +17,8 @@ import {XTokenInterceptor} from '@app/core/interceptors';
 import {AssetsInterceptor} from '@app/core/interceptors/assets.interceptor';
 import {AuthEffects, authReducer, metaReducer, RouterEffects} from '@app/auth/store';
 import {functionalErrorsReducer} from '@app/auth/store/errors.reducer';
+import {CacheService} from "@shared/services";
+import {APP_CONFIG} from "@helpers/constants";
 
 export const metaReducers: MetaReducer<any>[] = [initStateFromLocalStorage];
 
@@ -24,46 +26,57 @@ if (!environment.production) {
   metaReducers.unshift(debug);
 }
 
-@NgModule({ imports: [
-        // angular
-        CommonModule,
-        // ngrx
-        StoreModule.forRoot({
-            'core:auth:constantine': authReducer,
-            meta: metaReducer,
-            functionalErrors: functionalErrorsReducer,
-        }, {
-            metaReducers,
-            runtimeChecks: {
-                strictStateImmutability: false,
-                strictActionImmutability: false,
-                strictStateSerializability: false,
-                strictActionSerializability: false,
-                strictActionWithinNgZone: false,
-            },
-        }),
-        EffectsModule.forRoot([AuthEffects, RouterEffects]),
-        // Instrumentation must be imported after importing StoreModule
-        StoreDevtoolsModule.instrument({
-            maxAge: 25,
-            logOnly: environment.production,
-        })], providers: [
-        LocalStorageService,
-        AuthService,
-        // ngx-mask (Angular 16+)
-        provideNgxMask(),
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: XTokenInterceptor,
-            multi: true,
-        },
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AssetsInterceptor,
-            multi: true,
-        },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+@NgModule({
+  imports: [
+    // angular
+    CommonModule,
+    // ngrx
+    StoreModule.forRoot({
+      'core:auth:constantine': authReducer,
+      meta: metaReducer,
+      functionalErrors: functionalErrorsReducer,
+    }, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: false,
+        strictActionImmutability: false,
+        strictStateSerializability: false,
+        strictActionSerializability: false,
+        strictActionWithinNgZone: false,
+      },
+    }),
+    EffectsModule.forRoot([AuthEffects, RouterEffects]),
+    // Instrumentation must be imported after importing StoreModule
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    })],
+  providers: [
+    CacheService,
+    {
+      provide: APP_CONFIG,
+      useValue: {
+        debounceTime: 600,
+        snackDuration: 5000
+      }
+    },
+    LocalStorageService,
+    AuthService,
+    // ngx-mask (Angular 16+)
+    provideNgxMask(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: XTokenInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AssetsInterceptor,
+      multi: true,
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+  ]
+})
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     if (parentModule) {
